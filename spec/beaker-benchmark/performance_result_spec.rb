d5e02b9fcd3421ba7d114560801a357c038ed122
bill.claytor <bill.claytor@puppet.com>
2019-10-09 12:37:47 -0700

    (SLV-568) Re-enable 'disk_read' measurement

--- spec/beaker-benchmark/performance_result_spec.rb
+++ spec/beaker-benchmark/performance_result_spec.rb
@@ -12,9 +12,9 @@ end
 
 describe ClassMixedWithDSLHelpers do
   subject { Beaker::DSL::BeakerBenchmark::Helpers::PerformanceResult.new (
-            {:cpu => [60, 40], :mem => [6000, 4000], :disk_write => [600, 400], :action => 'test_action', :duration => 10,
-             :processes => {1000 => {:cmd => 'proc1', :cpu_usage => [10, 20], :mem_usage => [1000, 2000], :disk_write => [100, 200]},
-                            2000 => {:cmd => 'proc2', :cpu_usage => [20, 40], :mem_usage => [2000, 4000], :disk_write => [200, 400]}},
+            {:cpu => [60, 40], :mem => [6000, 4000], :disk_read => [600, 400], :disk_write => [600, 400], :action => 'test_action', :duration => 10,
+             :processes => {1000 => {:cmd => 'proc1', :cpu_usage => [10, 20], :mem_usage => [1000, 2000], :disk_read => [100, 200], :disk_write => [100, 200]},
+                            2000 => {:cmd => 'proc2', :cpu_usage => [20, 40], :mem_usage => [2000, 4000], :disk_read => [200, 400], :disk_write => [200, 400]}},
              :logger => logger, :hostname => 'my_host'}) }
 
   describe 'initialize' do
@@ -57,11 +57,11 @@ describe ClassMixedWithDSLHelpers do
       csv_file_content = file.read
       expected_content = <<-EOS
 Action,Duration,Avg CPU,Avg MEM,Avg DSK read,Avg DSK Write
-test_action,10,50,5000,,500
+test_action,10,50,5000,500,500
 
 Process pid,command,Avg CPU,Avg MEM,Avg DSK read,Avg DSK Write
-1000,'proc1',15,1500,,150
-2000,'proc2',30,3000,,300
+1000,'proc1',15,1500,150,150
+2000,'proc2',30,3000,300,300
       EOS
       expect(csv_file_content).to eq(expected_content)
     end
